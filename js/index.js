d43bf3a0710fd0f7d80e6752a734a8fabf2434fc
Duc <toduccuclun2008@gmail.com>
2024-11-21 09:38:41 +0700

    draworld

--- js/index.js
+++ js/index.js
@@ -25,3 +25,24 @@ logOut.addEventListener("click", () => {
       // An error happened.
     });
 });
+const searching = document.querySelector("#searching");
+const dropdownText = document.querySelector(".dropdown-text");
+const dropdownTextItem = document.querySelectorAll(".dropdown-text li");
+searching.addEventListener("keyup", () => {
+  dropdownText.style.display = "block";
+  const searchValue = searching.value.toUpperCase();
+  console.log(searchValue);
+  for (i = 0; i < dropdownTextItem.length; i++) {
+    const txt = dropdownTextItem[i].querySelectorAll("p")[0];
+    const txtValue = txt.textContent || txt.innerText;
+    if (txtValue.toUpperCase().indexOf(searchValue) > -1) {
+      dropdownTextItem[i].style.display = "";
+    } else {
+      dropdownTextItem[i].style.display = "none";
+    }
+  }
+});
+
+searching.addEventListener("blur", () => {
+  dropdownText.style.display = "none";
+});
--- js/interact.js
+++ js/interact.js
@@ -13,19 +13,3 @@ solidHeart.forEach((interaction) => {
     }
   });
 });
-function myFunction() {
-  var input, filter, ul, li, a, i, txtValue;
-  input = document.getElementById("searching");
-  filter = input.value.toUpperCase();
-  ul = document.getElementById("dropdown-text");
-  li = ul.getElementsByTagName("li");
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    txtValue = a.textContent || a.innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
-  }
-}
